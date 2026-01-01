0a39d688835e03298f2a5e9b694b318646242516
ZahranAlvan <zahranalvan2901@gmail.com>
2026-01-01 14:57:02 +0700

    fix: tambah SEO

--- alvacodee/src/app/layout.tsx
+++ alvacodee/src/app/layout.tsx
@@ -9,10 +9,22 @@ import ChatBot from "@/app/components/ui/ChatBot";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Alvacodee",
-  description: "Personal portfolio of Zahran Alvan",
+  title: "Zahran Alvan | Software Engineer | Data & AI/ML Enthusiast",
+  description: "Portfolio Zahran Alvan (Alvacodee). Computer Science student at ITB with Software Engineering, Data Science, and AI/ML focus.",
+  keywords: ["Zahran Alvan", "Alvacodee", "Portfolio", "Software Engineer", "ITB", "Informatika", "Computer Science", "Data Science", "AI/ML", "Web Developer"],
+  authors: [{ name: "Zahran Alvan" }],
+  verification: {
+      google: "KSfwgislrB98OUDIEN5SbJRYpA_wJ4DpQHeaCy-22zc",
+    },
+  // Thumbnail
+  openGraph: {
+    title: "Zahran Alvan - Portfolio",
+    description: "Connect with Zahran Alvan, a Software Engineer based in Indonesia.",
+    url: "https://alvacodee-web.vercel.app", // Ganti kalau nanti beli domain
+    siteName: "Zahran Alvan Portfolio",
+    type: "website",
+  },
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
